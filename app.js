28df8a081e10dfc9d9f318ab1dc3941430757af5
jevon-laptop <jevon.fillette@gmail.com>
2023-10-06 19:34:09 -0400

    Create Schemas and populate database

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://admin:libraryadmin1234@cluster0.isopug8.mongodb.net/local_library?retryWrites=true&w=majority";
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
