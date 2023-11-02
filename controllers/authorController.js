7b31a0c40b071199926bd557eef119b5740b5300
Code4Daze <jevon.fillette@gmail.com>
2023-11-01 21:30:04 -0400

    Start working on author delete

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -97,11 +97,41 @@ exports.author_create_post = [
 ];
 
 exports.author_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author delete GET");
+  const [author, allBooksByAuthor] = await Promise.all([
+    Author.findById(req.params.id).exec(),
+    Book.find({ author: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (author === null) {
+    res.redirect("/catalog/authors");
+  }
+
+  res.render('base', {
+    blockContent: "author_delete",
+    title: "Delete Author",
+    author: author,
+    author_books: allBooksByAuthor,
+  })
 });
 
 exports.author_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author delete POST");
+  const [author, allBooksByAuthor] = await Promise.all([
+    Author.findById(req.params.id).exec(),
+    Book.find({ author: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (allBooksByAuthor > 0) {
+    res.render('base', {
+      blockContent: "author_delete",
+      title: "Delete Author",
+      author: author,
+      author_books: allBooksByAuthor,
+    });
+    return;
+  } else {
+    await Author.findByIdAndRemove(req.body.author._id);
+    res.redirect("/catalog/authors");
+  }
 });
 
 exports.author_update_get = asyncHandler(async (req, res, next) => {
